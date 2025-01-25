9c0a723f1fb0a78851867da4115a36daf39462c7
Jackal <ccxxll23@163.com>
2025-01-25 16:15:28 +0800

    updated

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Link href="/operation">
+      <Link href="/operation" legacyBehavior>
         <a className="text-blue-500 underline">前往运维页面</a>
       </Link>
     </div>
   );
-} 
+}
